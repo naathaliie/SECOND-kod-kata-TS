a2f15f0bc863c0ebb6737e65de316e7f33cb74cd
naathaliie <nathalielarsson@live.se>
2024-03-06 15:19:40 +0100

    list + 2 utmaningar

--- src/List.tsx
+++ src/List.tsx
@@ -1,17 +1,28 @@
 /* Skapa komponenten List som har ett textfält, 
 knapp och ul. 
 När användaren klickar på knappen ska en ny punkt 
-läggas till i listan med det som stod i textfält */
+läggas till i listan med det som stod i textfält
 
-import { useState } from "react";
+UTMANING 1: Töm textfältet när man klickar på knappen 
+och sätt tillbaka fokus på textfältet. Tänk på taborder.
+
+UTMANING2: Lägg till en dropdown med ett antal emojis 
+på djur. När användaren klickar på knappen ska emojin 
+och texten läggas till listan.*/
+
+import { useRef, useState } from "react";
+//Alla emojisar
+const emojis = ["🐰", "🦢", "🦖", "🐌", "🦄"];
 
 const List = () => {
   const [inputWord, setInputWord] = useState("");
   const [words, setWords] = useState(["ett", "två", "tre"]);
+  const [selectedEmoji, setSelectedEmoji] = useState("null");
+  const emojiRef = useRef(null);
 
   const handleOnClick = () => {
     if (inputWord !== "") {
-      setWords([...words, inputWord]);
+      setWords([...words, inputWord + selectedEmoji]);
       setInputWord("");
 
       document.querySelector("input")?.focus();
@@ -20,24 +31,48 @@ const List = () => {
     }
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputWord(e.target.value);
+  };
+
+  const handleEmojiChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedEmoji(e.target.value);
   };
 
   return (
     <div className="listDiv">
       <h3>List</h3>
-      <div>
-        <input
-          type="text"
-          /*   ref={inputRef} */
-          value={inputWord}
-          onChange={handleChange}
-        />
+
+      <div className="allInputs">
+        <div className="inputListDiv">
+          <label htmlFor="inputList">Skriv ett ord</label>
+          <input
+            id="inputList"
+            type="text"
+            value={inputWord}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="emojisDiv">
+          <label htmlFor="emojis">Välj en emoji</label>
+          <select name="emojiDrop" id="emojis" onChange={handleEmojiChange}>
+            <option value="">---Emojis---</option>
+            {emojis.map((emoji, index) => {
+              return (
+                <option ref={emojiRef} key={index} value={`${emoji}`}>
+                  {emoji}
+                </option>
+              );
+            })}
+          </select>
+        </div>
       </div>
+
       <div>
         <button onClick={handleOnClick}>Lägg till i listan</button>
       </div>
+
       <div>
         <ul>
           {words.map((word, index) => {
