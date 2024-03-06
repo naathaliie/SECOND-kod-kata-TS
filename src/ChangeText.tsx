86caabe94d6c4532a803ee41460918e77ed50de2
naathaliie <nathalielarsson@live.se>
2024-03-06 10:04:28 +0100

    Utmaning 1 klar, ChangeText

--- src/ChangeText.tsx
+++ src/ChangeText.tsx
@@ -1,35 +1,54 @@
 /* En ny komponent ChangeText innehåller ett 
 input-fält för text, en knapp och en p-tagg. 
 När man klickar på knappen så kopieras texten 
-från input till p-taggen */
+från input till p-taggen 
+
+UTMANING: Ändra "kopiera" till "flytta" dvs 
+inputfältet töms. Sätt även fokus tillbaka på 
+inputfältet. Om textfältet är tomt ska inte 
+p-taggen ändras.*/
 
 import { useRef, useState } from "react";
 
 const ChangeText = () => {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [word, setWord] = useState("");
+  const [inputText, setInputText] = useState("");
+  const [displayText, setDisplayText] = useState("");
 
   const handleOnClick = () => {
-    if (inputRef.current?.value) {
-      console.log(inputRef.current.value);
-      setWord(inputRef.current.value);
+    if (inputText.trim() !== "") {
+      setDisplayText(inputText);
+      setInputText("");
+
+      if (inputRef.current) {
+        inputRef.current.focus(); //focus() sätter tabfokus på inputref
+      }
     } else {
       alert("App App App, skriv in ett ord först!");
     }
   };
 
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
   return (
     <div className="changeTextDiv">
       <div>
-        <input type="text" ref={inputRef} />
+        <input
+          type="text"
+          ref={inputRef}
+          value={inputText}
+          onChange={handleOnChange}
+        />
       </div>
       <div>
         <button onClick={handleOnClick}>Klicka mig</button>
       </div>
       <div>
         <p>
-          Ordet du skrev var:{" "}
-          <span style={{ fontWeight: "bolder" }}>{word}</span>
+          Ordet du skrev var:
+          <span style={{ fontWeight: "bolder" }}> {displayText}</span>
         </p>
       </div>
     </div>
