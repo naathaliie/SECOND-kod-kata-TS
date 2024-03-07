5e07c5001b75145a66e1aa4ccc98170a80426ea6
naathaliie <nathalielarsson@live.se>
2024-03-07 11:13:45 +0100

    PrettyText

--- src/PrettyText/PrettyText.tsx
+++ src/PrettyText/PrettyText.tsx
@@ -8,19 +8,24 @@ kryssrutorna formaterar texten. */
 
 import { useRef, useState } from "react";
 
+const fontStyleOptions = [];
+
 const PrettyText = () => {
-  //input text från start skall vara tom
-  //skriva något som vi ska samla in från input
-  //vid knapptryck skall input tömmas ochh text skall visas i p-tagg
   const inputRef = useRef<HTMLTextAreaElement>(null);
   const [displayText, setDisplayText] = useState("");
+  const [textStyle, setTextStyle] = useState("");
 
   const handleOnClick = () => {
     if (inputRef.current) {
+      setTextStyle('fontStyle: "italic"');
       setDisplayText(inputRef.current.value);
       inputRef.current.value = ""; // rensar textarea-fältet
       inputRef.current.focus(); // sätter tillbaka fokus på textarea-fältet
     }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("vald stil: ", e.target.value);
   };
 
   return (
@@ -38,23 +43,50 @@ const PrettyText = () => {
         ></textarea>
       </div>
       <div className="checkboxes">
-        <input type="checkbox" id="fet" name="fet" />
+        <input
+          type="checkbox"
+          id="fet"
+          name="fet"
+          onChange={handleChange}
+          value={"fet"}
+        />
         <label htmlFor="fet">Fet</label>
 
-        <input type="checkbox" id="kursiv" name="kursiv" />
+        <input
+          type="checkbox"
+          id="kursiv"
+          name="kursiv"
+          onChange={handleChange}
+          value={"kursiv"}
+        />
         <label htmlFor="kursiv">kursiv</label>
 
-        <input type="checkbox" id="biggerText" name="biggerText" />
+        <input
+          type="checkbox"
+          id="biggerText"
+          name="biggerText"
+          onChange={handleChange}
+          value={"biggerText"}
+        />
         <label htmlFor="biggerText">större text</label>
       </div>
       <div>
         <button onClick={handleOnClick}>Klicka mig</button>
       </div>
       <div>
-        <p>Din text: {displayText}</p>
+        <p>
+          Din text: <span style={{ fontStyle: "italic" }}>{displayText}</span>
+        </p>
       </div>
     </div>
   );
 };
 
 export default PrettyText;
+
+/*  fontStyle: "italic"  
+fontWeight: "bolder"
+fontSize: "larger" 
+*/
+
+/* HUR skall jag göra för att sätta de gjorda style-valen till texten??? */
